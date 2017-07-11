3e0998641a682a1476545611b722dd7c669bac57
ThiagoWhispher <thiagoyeds2013@gmail.com>
2017-07-11 05:11:20 -0300

    Upgrade script

--- start1.sh
+++ start1.sh
@@ -3,24 +3,13 @@
 #__BEGIN__
 #Original content copyright (c) 2014 dpen2000 licensed under the MIT license
 
-#Upgrading npm
+#Message
 echo "reboot server..."
+
+#Upgrade npm
 sudo npm install -g npm@latest # upgrade npm
-sudo npm install -g bower
-sudo npm install -g brunch
-sudo npm install -g geoip-lite
-sudo npm install -g nodemon
 
-#bin /vagrant/node_modules so that it does not leak through to the host file system
-#which triggers symlink and path size issues on Windows hosts
-sudo mkdir -p /vagrant/node_modules
-sudo chown -R vagrant:vagrant /node_modules
+#Mount node_modules in vagrant
 sudo mount --bind /node_modules /vagrant/node_modules
-
-#prepare
-find /vagrant/app -type f -exec dos2unix {} \;
-find /vagrant/vendor -type f -exec dos2unix {} \;
-find /vagrant/scripts/vagrant -type f -exec dos2unix {} \;
-sudo chown -R vagrant:vagrant /home/vagrant
 
 #__END_MAIN__
